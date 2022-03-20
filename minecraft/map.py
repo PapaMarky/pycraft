80534161739e92c7f637e789844551a2af04eb0c
Mark Dyer <heymarky@gmail.com>
2022-03-19 19:19:42 -0700

    Move banner drawing to mapper.py so that text doesn't truncate

--- minecraft/map.py
+++ minecraft/map.py
@@ -1,13 +1,9 @@
 
 from minecraft.dat_file import DatFile
 from minecraft.colors import colors as color_map
-from minecraft.colors import get_color
 import json
 import sys
 from PIL import Image
-from PIL import ImageDraw
-from PIL import ImageFont
-from PIL import ImageColor
 
 # info about map scaling:
 # https://minecraft.fandom.com/wiki/Map#Zoom_details
@@ -64,7 +60,7 @@ class Map(DatFile):
         width = 128 if lencolors == 16384 else math.sqrt(lencolors)
         return width
         
-    def create_image(self, scale=1.0, banners=True):
+    def create_image(self, scale=1.0):
         width = self.get_width()
         img = Image.new(mode="RGBA", size=(width, width))
         print(f'WIDTH: {width}')
@@ -93,49 +89,5 @@ class Map(DatFile):
 
         if scale != 1.0:
             img = img.resize((int(width * scale), int(width * scale)))
-        if banners:
-            zoom = self.get_zoom()
-            center = self.get_center()
-            bpp = (1, 2, 4, 8, 16)[zoom]/ scale
-            map_origin = self.get_origin()
-            blocks_wide = self.width_in_blocks()
-            image_size = img.width
-            #font = ImageFont.truetype("Tests/fonts/NotoSans-Regular.ttf", 48)
-            font = ImageFont.truetype('Keyboard.ttf')
-            def draw_banner(pos, color, name, img):
-                ox = center[0] - (blocks_wide/2)
-                oz = center[1] - (blocks_wide/2)
-                print(f' map center: {center}')
-                print(f'     origin: ({ox}, {oz})')
-                print(f'        pos: ({pos[0]}, {pos[2]})')
-                print(f' COLOR: "{color}"')
-                x_pct = (pos[0] - ox) / blocks_wide
-                y_pct = (pos[2] - oz) / blocks_wide
-                print(f'offset: ({x_pct}, {y_pct})')
-                pix_x = int(x_pct * image_size)
-                pix_y = int(y_pct * image_size)
-                print(f'img size: {image_size}')
-                print(f'img pos: ({pix_x}, {pix_y})')
-                poly_pts = (
-                    (pix_x, pix_y),
-                    (pix_x + 3, pix_y - 3),
-                    (pix_x + 3, pix_y - 8),
-                    (pix_x - 3, pix_y - 8),
-                    (pix_x - 3, pix_y - 3)
-                )
-                banner_color = get_color(color)
-                print(f'BANNER COLOR: {banner_color}')
-                draw = ImageDraw.Draw(img)
-                draw.polygon(poly_pts, fill=banner_color, outline='black')
 
-                if name:
-                    draw.text((pix_x-1, pix_y-1), name, fill='black', anchor='ma', font=font)
-                    draw.text((pix_x+1, pix_y+1), name, fill='black', anchor='ma', font=font)
-                    draw.text((pix_x, pix_y), name, fill='white', anchor='ma', font=font)
-
-            banner_data = self.get_banners()
-            if len(banner_data) > 0:
-                print(f'BANNERS:')
-                for b in banner_data:
-                    draw_banner(b['pos'], b['color'], b['name'], img)
         return img
