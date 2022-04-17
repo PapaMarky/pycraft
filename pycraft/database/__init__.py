0009993a984d35d317b1e59d9c9d23b79ff09f96
Mark Dyer <heymarky@gmail.com>
2022-04-17 11:54:14 -0700

    Use SQLalchemy better. Prepare for multithreading

--- pycraft/database/__init__.py
+++ pycraft/database/__init__.py
@@ -3,6 +3,7 @@ Load Minecraft data into a relational database to simplify subsequent data queri
 '''
 import sqlalchemy as db
 import threading
+import logging
 
 class Database():
     _RECORD_ID = 0
@@ -10,8 +11,7 @@ class Database():
     def __init__(self, dbfile):
         self._engine = db.create_engine(f'sqlite:///{dbfile}')
         self._connection = self._engine.connect()
-        self._metadata = db.MetaData()
-        self._tables = {}
+        self._metadata = db.MetaData(bind=self._engine)
         self._create_tables()
 
     def next_record_id():
@@ -32,13 +32,16 @@ class Database():
         self.insert_records('item_modifiers', records)
 
     def insert_records(self, table, records):
-        query = db.insert(self._tables[table])
-        ResultProxy = self._connection.execute(query, records)
+        connection = self._engine.connect()
+        table_obj = self._metadata.tables[table]
+        query = db.insert(table_obj)
+        ResultProxy = connection.execute(query, records)
 
     def _create_item_table(self):
+        logging.info('Create Items Table')
         items = db.Table(
             'items', self._metadata,
-            db.Column('Id', db.Integer(), nullable=False),
+            db.Column('Id', db.Integer(), nullable=False, primary_key=True),
             db.Column('Owner', db.String(32)),
             db.Column('Container', db.String(32)), # chest, hand, armor, ...
             db.Column('container_item', db.Integer()),
@@ -47,11 +50,12 @@ class Database():
             db.Column('z', db.Integer(), nullable=False),
             db.Column('type', db.String(32), nullable=False),
             db.Column('count', db.Integer(), nullable=False),
-            db.Column('slot', db.Integer(), nullable=False)
+            db.Column('slot', db.Integer(), nullable=False),
+            keep_existing=True
         )
-        self._tables['items'] = items
 
     def _create_entity_table(self):
+        logging.info('Create Entities Table')
         entities = db.Table(
             'entities', self._metadata,
             db.Column('Id', db.String(32), nullable=False),
@@ -63,21 +67,23 @@ class Database():
             db.Column('color', db.String(32)),
             db.Column('chested', db.Boolean()),
             db.Column('tame', db.Boolean()),
-            db.Column('owner', db.String(32))
+            db.Column('owner', db.String(32)),
+            keep_existing=True
         )
-        self._tables['entities'] = entities
 
     def _create_item_modifiers_table(self):
+        logging.info('Create Item Modifiers Table')
         item_modifiers = db.Table(
             'item_modifiers', self._metadata,
             db.Column('item_id', db.Integer(), nullable=False),
             db.Column('modifier', db.String(32), nullable=False),
             db.Column('value', db.Integer(), nullable=False),
-            db.Column('type', db.String(32))
+            db.Column('type', db.String(32)),
+            keep_existing=True
         )
-        self._tables['item_modifiers'] = item_modifiers
 
     def _create_villager_table(self):
+        logging.info('Create Villager Table')
         villagers = db.Table(
             'villagers', self._metadata,
             db.Column('Id', db.String(32), nullable=False),
@@ -87,13 +93,14 @@ class Database():
             db.Column('home_z', db.Integer()),
             db.Column('meet_x', db.Integer()),
             db.Column('meet_y', db.Integer()),
-            db.Column('meet_z', db.Integer())
+            db.Column('meet_z', db.Integer()),
+            keep_existing=True
         )
-        self._tables['villagers'] = villagers
 
     def _create_tables(self):
         self._create_entity_table()
         self._create_villager_table()
         self._create_item_table()
         self._create_item_modifiers_table()
-        self._metadata.create_all(self._engine)
+        self._metadata.create_all(checkfirst=True)
+        self._metadata.reflect()
