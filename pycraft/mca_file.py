fb0e4315928f2288e189af2787a26c1551c7b2e9
Mark Dyer <heymarky@gmail.com>
2022-04-19 15:38:40 -0700

    Expose the size of a chunks data as a property

--- pycraft/mca_file.py
+++ pycraft/mca_file.py
@@ -15,8 +15,13 @@ class McaFile:
         if not os.path.exists(self._path):
             raise PycraftException(f'McaFile: file not found: "{self._path}"')
         self._mca = mca.Pycrafta(self._path)
-        
+
+    def get_chunk_size(self, chunk_x, chunk_y):
+        if not self._mca:
+            raise PycraftException('McaFile: Not open')
+        return self._mca.get_data_size(chunk_x, chunk_y)
+
     def get_chunk(self, chunk_x, chunk_y):
         if not self._mca:
             raise PycraftException('McaFile: Not open')
-        return Chunk(self._mca.get_data(chunk_x, chunk_y))
+        return Chunk(self._mca.get_data(chunk_x, chunk_y, self._mca.get_data_size(chunk_x, chunk_y)))
